cb63bec9d9899a83ba8593c7947c8b550d2d8e66
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-08-22 16:56:56 +0530

    commented sliver test

--- lib/userInterface/testCodes.dart
+++ lib/userInterface/testCodes.dart
@@ -25,3 +25,35 @@
 
 //   _animateToIndex(i) => _controller.animateTo(_height * i, duration: Duration(seconds: 2), curve: Curves.fastOutSlowIn);
 // }
+
+
+
+// body: CustomScrollView(
+        //   slivers: <Widget>[
+        //     SliverAppBar(
+        //       expandedHeight: 200,
+        //       title: Text("Docker"),
+        //       flexibleSpace: FlexibleSpaceBar(
+        //         background: Image.asset("assets/images/dockerHome.png"),
+        //       ),
+        //     ),
+        //     SliverList(
+        //       delegate: SliverChildBuilderDelegate((context, index) => 
+        //         Card(
+        //           child: Container(
+        //             padding: EdgeInsets.all(10.0),
+        //             child: Row(
+        //               mainAxisAlignment: MainAxisAlignment.start,
+        //               children: <Widget>[
+        //                 CircleAvatar(
+        //                   backgroundColor: Colors.black,
+        //                 ),
+        //                 Text("Docker"),
+        //               ],
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //   ],
+        // ),
