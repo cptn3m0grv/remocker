08f6fc3b2d18b8a6670640e341e5679a4e423003
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-08-22 17:16:43 +0530

    Started working on docker home page

--- lib/userInterface/dockerPage.dart
+++ lib/userInterface/dockerPage.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-
+import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
 
 String input;
@@ -11,38 +11,162 @@ class DockerHome extends StatefulWidget{
 }
 
 class _DockerHome extends State<DockerHome>{
+
+  var inputFromUser;
+
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              expandedHeight: 200,
-              title: Text("Docker"),
-              flexibleSpace: FlexibleSpaceBar(
-                background: Image.asset("assets/images/dockerHome.png"),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate((context, index) => 
-                Card(
-                  child: Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Color(0xff085F63),
+        title: Text(
+          "Docker Services",
+          style: TextStyle(
+            fontFamily: 'Ubuntu',
+            fontSize: 25,
+          ),
+        )
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Color(0xff32dbc6),
+              child: Column(
+                children: [
+                  Card(
+                    child: TextField(
+                      onChanged: (value) {
+                        inputFromUser = value;
+                      },
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      cursorColor: Colors.black,
+                      decoration: InputDecoration(
+                        fillColor: Color(0xffebefd0),
+                        hintText: 'Enter Command',
+                        hintStyle: TextStyle(
+                          fontFamily: 'Ubuntu',
                         ),
-                        Text("Docker"),
-                      ],
+                        prefixIcon: Icon(Icons.arrow_forward),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          )
+                        ),
+                        contentPadding: EdgeInsets.all(10),
+                      ),
                     ),
                   ),
-                ),
+                  Text("    "),
+                  FloatingActionButton.extended(
+                    onPressed: () {
+                      print("Run pressed");
+                      // getOutput(inputFromUser);
+                    },
+                    backgroundColor: Color.fromARGB(200, 2, 20, 5),
+                    label: Text(
+                      "  Run  ",
+                      style: TextStyle(
+                        fontFamily: 'Ubuntu',
+                        fontSize: 20,
+                      ),
+                    ),
+                    icon: Icon(Icons.directions_run),
+                  ),
+                  Text("   "),
+                  DottedLine(
+                    dashLength: 20,
+                  ),
+                  Text("   "),
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Card(
+                    child: Text(
+                      "  Most Common Commands  ",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Ubuntu',
+                      ),
+                    ),
+                  ),
+                  Text(" "),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Card(
+                            child: FlatButton(
+                              onPressed: null,
+                              child: Text(
+                                "Availaible Images",
+                                style: TextStyle(
+                                  fontFamily: 'Ubuntu',
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Card(
+                            child: FlatButton(
+                              onPressed: null,
+                              child: Text(
+                                "Active Processes",
+                                style: TextStyle(
+                                  fontFamily: 'Ubuntu',
+                                  fontSize: 14,
+                                ),
+                              )
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Card(
+                            child: FlatButton(
+                              onPressed: null,
+                              child: Text(
+                                "All Processes",
+                                style: TextStyle(
+                                  fontFamily: 'Ubuntu',
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Card(
+                            child: FlatButton(
+                              onPressed: null,
+                              child: Text(
+                                "Docker Version",
+                                style: TextStyle(
+                                  fontFamily: 'Ubuntu',
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
         ),
+      ),
     );
   }
 }
